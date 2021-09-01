e2f9bcc6381243bff075c07842855fb840fc2cf9
ryanlow-tw <ryan.low@thoughtworks.com>
2021-09-02 00:24:51 +0800

    refactor analytics.py

--- analytics.py
+++ analytics.py
@@ -2,23 +2,20 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from numpy.polynomial.polynomial import polyfit
+
 plt.style.use('ggplot')
 
 df = pd.read_csv('results.csv')
 x = df["size"]
 y = df["average_time"]
-c, m = polyfit(x,y,1)
 
-size = 1000
+intercept, gradiant = polyfit(x, y, 1)
+get_predicted_duration_seconds = lambda filesize: gradiant * filesize + intercept
 
-def get_predicted_duration_seconds(filesize):
-    return m * filesize + c
-
-print(f"{size}MB data would take {get_predicted_duration_seconds(size)}")
-
-sns.scatterplot(x="size",y="average_time",data=df, color="#D47674")
 bestfit_x = [df["size"].min(), df["size"].max()]
 bestfit_y = list(map(get_predicted_duration_seconds, bestfit_x))
+
+sns.scatterplot(x="size",y="average_time",data=df, color="#D47674")
 plt.plot(bestfit_x, bestfit_y, linestyle="dashed", color="#54A9EB")
 plt.title("Performance curve of airflow pipeline")
 plt.ylabel("Average Duration (Seconds)")
