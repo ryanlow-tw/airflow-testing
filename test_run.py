12e97eac8d48c52dc0141fcf084bef606eded13a
ryanlow-tw <ryan.low@thoughtworks.com>
2021-09-02 15:17:36 +0800

    changed named arguments

--- test_run.py
+++ test_run.py
@@ -2,6 +2,6 @@ from run import get_airflow_run_duration
 
 
 def test_that_100MB_wordcount_job_should_not_be_more_than_45_seconds():
-    duration = get_airflow_run_duration(num_runs=1, size="100MB")
+    duration = get_airflow_run_duration(num_runs=1, filesize=1000000)
     assert duration <= 45
     
