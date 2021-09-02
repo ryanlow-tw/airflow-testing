aa3f0a2e8f40f4ff878d53d72a4f6eef420c93b4
ryanlow-tw <ryan.low@thoughtworks.com>
2021-09-02 15:17:15 +0800

    refactor run.py

--- run.py
+++ run.py
@@ -7,29 +7,33 @@ now = datetime.now()
 filename = f'logs/airflow-logs{now}-{now.hour}-{now.minute}-{now.second}'
 logging.basicConfig(filename=filename, encoding='utf-8', level=logging.DEBUG)
 
-def get_airflow_run_duration(num_runs, size):
-    start = time.time()
+def get_airflow_run_duration(num_runs, filesize):
+    duration = 0
     for i in range(num_runs):
         print(f"This is run number {i+1}")
-        bash_command = f"airflow tasks test wordcount_{size}_dag count_words_{size}_task 2021-08-30"
-        stream = os.popen(bash_command)
-        logging.debug(stream.read())
-    end = time.time()
-    return end - start
+        run_bash_command(f"airflow variables set filesize {filesize}")
+        start = time.time()
+        run_bash_command("airflow tasks test wordcount_dag count_words_task 2021-08-30")
+        end = time.time()
+        duration += (end - start)
+    return round((duration / num_runs), 2)
 
 def write_file(input_1, input_2):
     with open('results.csv', 'a') as f:
         f.write(f"{input_1},{input_2}\n")
 
+def run_bash_command(command):
+    stream = os.popen(command)
+    logging.debug(stream.read())
+
 def main():
-    filesizes = ["1MB", "5MB", "10MB", "20MB","100MB"]
+    filesizes = [10000,20000,50000,100000,200000,500000,1000000,2000000,5000000,10000000]
     NUM_RUNS = 3
     write_file(input_1="size", input_2="average_time")
-    for size in filesizes:
-        average_time = get_airflow_run_duration(num_runs=NUM_RUNS, size=size)
-        print(f"Time taken for wordcount, filesize: {size} is {average_time:.2f} seconds")
-        size_without_mb = size[:-2]
-        write_file(input_1=size_without_mb, input_2=average_time)
+    for filesize in filesizes:
+        average_time = get_airflow_run_duration(num_runs=NUM_RUNS, filesize=filesize)
+        print(f"Time taken for wordcount, filesize: {filesize} is {average_time:.2f} seconds")
+        write_file(input_1=filesize, input_2=average_time)
 
 if __name__ == "__main__":
     main()
